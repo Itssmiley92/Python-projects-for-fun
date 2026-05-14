2d4162aca77761a6362bdd5514ad4fb3a6181a28
Nolan Kelly <nolansk1@outlook.com>
2026-05-14 10:20:02 -0400

    Added computers choice to output.

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -9,10 +9,13 @@ option_3 = "Scissors"
 my_vars = [option_1, option_2, option_3]
 
 print("Rock Paper Scissors")
+sleep(0.5)
 gameStart = input("Do you want to play? (y/n)")
 while(gameStart != "y"):
     gameStart = input("Do you want to play? (y/n)")
+sleep(0.5)
 print("Great! Let's get started.")
+sleep(0.5)
 userChoice = input("Please enter your choice in lowercase (rock, paper, scissors): ")
 while(userChoice != "rock" and userChoice != "paper" and userChoice != "scissors"):
     userChoice = input("Please enter your choice in lowercase(rock, paper, scissors): ")
@@ -20,23 +23,32 @@ print(f"Your choice: {userChoice}")
 compChoice = random.choice(my_vars)
 if userChoice == "rock":
     if compChoice == "Rock":
+        print("Computer choice: Rock")
         print("It's a tie!")
     elif compChoice == "Paper":
+        print("Computer choice: Paper")
         print("You lose!")
     elif compChoice == "Scissors":
+        print("Computer choice: Scissors")
         print("You win!")
 elif userChoice == "paper":
     if compChoice == "Rock":
+        print("Computer choice: Rock")
         print("You win!")
     elif compChoice == "Paper":
+        print("Computer choice: Paper")
         print("It's a tie!")
     elif compChoice == "Scissors":
+        print("Computer choice: Scissors")
         print("You lose!")
 elif userChoice == "scissors":
     if compChoice == "Rock":
+        print("Computer choice: Rock")
         print("You lose!")
     elif compChoice == "Paper":
+        print("Computer choice: Paper")
         print("You win!")
     elif compChoice == "Scissors":
+        print("Computer choice: Scissors")
         print("It's a tie!")
 print("Good game! Thanks for playing!")
